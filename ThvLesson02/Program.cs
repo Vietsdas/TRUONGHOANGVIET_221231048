77916ebde208fb6af84a363e7f9416c454c6f915
Vietsdas <hoangvietbk2004@gmail.com>
2026-08-23 23:35:35 +0700

    ThvLesson2

---
+++ ThvLesson02/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ThvLesson02
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello, Trương Hoàng Việt!");
+
+            app.Run();
+        }
+    }
+}
---
+++ ThvLesson2MVC/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ThvLesson2MVC.Models;
+
+namespace ThvLesson2MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.name = "Class UTC.K65CNTT2.LTW";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
