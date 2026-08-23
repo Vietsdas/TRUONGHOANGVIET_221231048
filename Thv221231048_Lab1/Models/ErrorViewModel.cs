b5313eb3d29affb4a0bf44c47343baf7309c298a
Vietsdas <hoangvietbk2004@gmail.com>
2026-08-24 00:12:00 +0700

    Thv221231048_Lab1

---
+++ Thv221231048_Lab1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Thv221231048_Lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
